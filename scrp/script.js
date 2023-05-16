a0fe82a4a706e03e8e69c08212e44b2df5ae4d40
andre <133234621+andreribeirogit@users.noreply.github.com>
2023-05-16 10:37:33 -0300

    versao1
    primeira versão

---
+++ scrp/script.js
@@ -0,0 +1,17 @@
+function calcularM(){
+   alert("Dados cadastrados com Sucesso!!!")
+   location.href = "index2.html"
+}
+function calcular1(){
+    alert("Esse vestido custa R$99,90")
+    alert("Entre em contato no Whatzapp (61)9999-8888")
+
+ }
+ function calcular2(){
+    alert("Esse vestido custa R$129,90")
+    alert("Entre em contato no Whatzapp (61)9999-8888")
+ }
+ function calcular3(){
+    alert("Esse vestido custa R$189,90")
+    alert("Entre em contato no Whatzapp (61)9999-8888")
+ }
